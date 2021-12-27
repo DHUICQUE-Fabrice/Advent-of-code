658002ffcb9cf573c911df10b7384a0cb54a49f6
DHUICQUE-Fabrice <fabrice.dhuicque2021@campus-eni.fr>
2021-12-27 16:57:52 +0100

    Day 2 - 2017

--- src/fr/aelhan/aoc2017/December02.java
+++ src/fr/aelhan/aoc2017/December02.java
@@ -10,19 +10,48 @@ public class December02 {
 	GeneralTools generalTools = new GeneralTools();
 
 	public void format(String input) {
-		String formatted = input;
+		int[][] formatted = fileReader.toIntArray2D(input, "\r\n", "	");
 		solvePart1(formatted);
 		solvePart2(formatted);
 	}
 
-	public void solvePart1(String input) {
-
-		System.out.println("Part 1 response : ");
+	public void solvePart1(int[][] input) {
+		int answer = 0;
+		for (int i = 0; i < input.length; i++) {
+			int max = Integer.MIN_VALUE;
+			int min = Integer.MAX_VALUE;
+			for (int j = 0; j < input[i].length; j++) {
+				if (input[i][j] > max) {
+					max = input[i][j];
+				}
+				if (input[i][j] < min) {
+					min = input[i][j];
+				}
+			}
+			answer += max - min;
+		}
+		System.out.println("Part 1 response : " + answer);
 	}
 
-	public void solvePart2(String input) {
-
-		System.out.println("Part 2 response : ");
+	public void solvePart2(int[][] input) {
+		int answer = 0;
+		for (int i = 0; i < input.length; i++) {
+			for (int j = 0; j < input[i].length - 1; j++) {
+				int toCheck1 = input[i][j];
+				for (int k = j + 1; k < input.length; k++) {
+					int toCheck2 = input[i][k];
+					if (toCheck1 % toCheck2 == 0) {
+						answer += toCheck1 / toCheck2;
+						break;
+					}
+					if (toCheck2 % toCheck1 == 0) {
+						answer += toCheck2 / toCheck1;
+						break;
+					}
+				}
+			}
+		}
+		System.out.println("Part 2 response : " + answer);
 	}
 
 }
