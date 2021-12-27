104745bc5b68a1e4ae7c8d0d80250b8b74e8b158
DHUICQUE-Fabrice <fabrice.dhuicque2021@campus-eni.fr>
2021-12-27 16:47:43 +0100

    Day 1 - 2017

--- src/fr/aelhan/aoc2017/December01.java
+++ src/fr/aelhan/aoc2017/December01.java
@@ -10,19 +10,38 @@ public class December01 {
 	GeneralTools generalTools = new GeneralTools();
 
 	public void format(String input) {
-		String formatted = input;
+		int[] formatted = fileReader.toIntArray1D(input, "");
 		solvePart1(formatted);
 		solvePart2(formatted);
 	}
 
-	public void solvePart1(String input) {
-
-		System.out.println("Part 1 response : ");
+	public void solvePart1(int[] input) {
+		int answer = 0;
+		for (int i = 0; i < input.length - 1; i++) {
+			if (input[i] == input[i + 1]) {
+				answer += input[i];
+			}
+		}
+		if (input[0] == input[input.length - 1]) {
+			answer += input[0];
+		}
+		System.out.println("Part 1 response : " + answer);
 	}
 
-	public void solvePart2(String input) {
-
-		System.out.println("Part 2 response : ");
+	public void solvePart2(int[] input) {
+		int answer = 0;
+		for (int i = 0; i < input.length; i++) {
+			try {
+				if (input[i] == input[input.length / 2 + i]) {
+					answer += input[i];
+				}
+			} catch (Exception e) {
+				if (input[i] == input[i - input.length / 2]) {
+					answer += input[i];
+				}
+			}
+		}
+		System.out.println("Part 2 response : " + answer);
 	}
 
 }
